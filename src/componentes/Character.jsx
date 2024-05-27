ed01f004726dbfa5e54e9a4dd4369b6545cb80e8
N3ptune4197 <carloschero134@gmail.com>
2024-05-27 17:17:12 -0500

    Second commit: organize cards

--- src/componentes/Character.jsx
+++ src/componentes/Character.jsx
@@ -6,14 +6,14 @@ import React from "react";
 
             {
                 characters.map((item, index) => (
-                    <div key={index} className="card w-72 md:w-80 lg:w-80 xl:w-96  bg-base-100 shadow-xl">
+                    <div key={index} className="card w-72 md:w-80 lg:w-80 xl:w-80  bg-base-100 shadow-xl">
                         <figure><img src={item.image} alt="Shoes" className="w-58" /></figure>
                         <div className="card-body">
-                            <h2 className="card-title text-2xl md:text-3xl">
+                            <h2 className="card-title text-xl md:text-3xl text-center">
                                 {item.name}
                             </h2>
-                            <p className="text-lg">Especie: {item.species}</p>
-
+                            <p className="text-lg">Specie: {item.species}</p>
+                            <div className="divider">TAGS</div>
                             <div className="card-actions justify-end">
                                 <div className="badge badge-primary badge-md">{item.status}</div> 
                                 <div className="badge badge-secondary">{item.gender}</div>
--- src/componentes/SectionTwo.jsx
+++ src/componentes/SectionTwo.jsx
@@ -3,15 +3,15 @@ import React from "react";
 
 export default function SectionTwo({children}) {
     return(
-            <section className="flex flex-col bg-blue-950">
+            <section className="flex flex-col bg-blue-950 pt-5">
 
-                <div className="join mt-12 justify-center">
+                <div className="join mt-16 justify-center mb-10">
                     <button className="join-item btn">«</button>
                     <button className="join-item btn">Página 1</button>
                     <button className="join-item btn">»</button>
                 </div>
 
-                <div className="grid p-18 md:p-14 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-7 justify-center pt-16">
+                <div className="grid p-18 md:p-14 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 gap-4 md:gap-7 justify-center items-center">
                     
                     {children}
 
